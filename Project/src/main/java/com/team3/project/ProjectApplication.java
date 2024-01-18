f7d1a70320756f93fd0e8d7e29e6eccef7e1e23c
LK <ae2207@uni-rostock.de>
2024-01-18 16:53:16 +0100

    Setting changes Web Application auf localhost:8080

--- Project/src/main/java/com/team3/project/ProjectApplication.java
+++ Project/src/main/java/com/team3/project/ProjectApplication.java
@@ -3,6 +3,7 @@ package com.team3.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,12 @@ public class ProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
+    }
+
+    // Start Page (static)
+    @RequestMapping()
+    public String index() {
+        return "index.html";
     }
 
     @GetMapping("/hello")
