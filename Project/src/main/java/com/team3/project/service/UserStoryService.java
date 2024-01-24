41d84ed32e7803a4d563f21c8e4af16c9bea8110
LK <ae2207@uni-rostock.de>
2024-01-24 19:06:25 +0100

    Erstellen von UserStoryService.java Setup

---
+++ Project/src/main/java/com/team3/project/service/UserStoryService.java
@@ -0,0 +1,23 @@
+package com.team3.project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserStoryService {
+
+    // TODO: needs Database connection to implement create Object
+    public boolean CreateUserStory(String UserStoryStory){
+        return false;
+    }
+
+    // TODO: needs Database connection to implement get Object
+    public <t> Object ChooseUserStory(int UserStoryID){
+        return null;
+    }
+
+    // TODO: needs Database connection to implement Update Object
+    public boolean UpdateUserStory(int UserStoryID, String UserStoryStory, int Priority){
+        return true;
+    }
+
+}
