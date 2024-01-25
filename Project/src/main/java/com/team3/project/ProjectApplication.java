cfc9a295de07f7ac37303db50da1c80c8c3b48e8
LK <ae2207@uni-rostock.de>
2024-01-25 12:07:55 +0100

    Bugfix

--- Project/src/main/java/com/team3/project/ProjectApplication.java
+++ Project/src/main/java/com/team3/project/ProjectApplication.java
@@ -1,24 +1,29 @@
 package com.team3.project;
 
 import com.team3.project.service.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @SpringBootApplication
 @Controller
 public class ProjectApplication {
 
+    public ProjectApplication(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
     }
 
-    @Autowired
-    private AccountService accountService;
+    private final AccountService accountService;
 
+    // TODO: IOExeptions
     // Start Seite (static)
     // TODO: ersetze index.html mit Login Page
     @RequestMapping(value = "/")
@@ -28,17 +33,16 @@ public class ProjectApplication {
 
     // TODO: implement Login.check.Database(UName,Pw) in service
     // TODO: create Template for User Page
-    // FIXME: Ersetze ?myName (GET) Typ von Request durch POST Typ
 
     @RequestMapping(value ="/Login", method = RequestMethod.POST)
     @ResponseBody
     public String Login(String Username, String Passwort) {
-        return accountService.LoginCheck(Username, Passwort) ? String.format("Hello %s! Dein PW: %s!!", Username, Passwort) : String.format("Wrong Username or Passwort");
+        return accountService.LoginCheck(Username, Passwort) ? String.format("Hello %s! Dein PW: %s!!", Username, Passwort) : "Wrong Username or Passwort";
     }
     @RequestMapping(value = "/Register", method = RequestMethod.POST)
     @ResponseBody
     public String Register(String EMail, String Passwort){
-        return accountService.CreateAccount(EMail,Passwort) ? String.format("Moin, du bis registriert!!") : String.format("Deine E-Mail ist bereits mit einem Account verbunden.") ;
+        return accountService.CreateAccount(EMail,Passwort) ? "Moin, du bis registriert!!" : "Deine E-Mail ist bereits mit einem Account verbunden." ;
     }
 
 }
