5f3be0486061347ddd8c7282c713ae5c68c12682
LK <ae2207@uni-rostock.de>
2024-02-04 12:00:28 +0100

    BugFix

--- Project/src/main/java/com/team3/project/ProjectApplication.java
+++ Project/src/main/java/com/team3/project/ProjectApplication.java
@@ -1,6 +1,7 @@
 package com.team3.project;
 
 import com.team3.project.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -42,7 +43,7 @@ public class ProjectApplication {
 
 
     @RequestMapping(value = "/RegisterPage")
-    public ModelAndView Register(){
+    public ModelAndView RegisterPage(){
        ModelAndView modelAndView = new ModelAndView("register");
        return modelAndView;
     }
