0d051243d638b6d1406b551e16aeebba74b1f876
HenryF <wx2225@uni-rostock.de>
2024-02-04 10:30:23 +0100

    Grundstruktur + Exception in AccountService

--- Project/src/main/java/com/team3/project/service/AccountService.java
+++ Project/src/main/java/com/team3/project/service/AccountService.java
@@ -5,16 +5,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class AccountService {
     //Alternative um ohne DB zu schreiben
-    public boolean found(String Mail){
+    public boolean check(String Mail){
         return !Mail.equals("test@gmail.com");
     }
     public boolean LoginCheck(String UName, String PW){
         return !UName.equals("Fail");
     }
-    public boolean CreateAccount(String Username, String Mail, String passwort){
-        //TODO: send Mail to DB-Layer and check if already in DB
-        //provisorisch mit found imitiert
-        return !found(Mail);
+    public boolean CreateAccount(String Username, String mail, String passwort){
+
+            //TODO:DB-Abfrage and check if already in DB
+            //TODO: Methode für Anfrage an DB ein Account zu erstellen
+            //if(check(Mail) == true){catch(Exception e){ throw new SecurityException("Deine E-Mail ist mit einem anderen Account verknüpft");}
+            //else{createAccountDB(username, mail, passwort);}
+
+            //provisorisch mit eigener check-Methode imitiert
+        return !check(mail);
     }
 
     public void setPasswort(String EMail, String Passwort) {
