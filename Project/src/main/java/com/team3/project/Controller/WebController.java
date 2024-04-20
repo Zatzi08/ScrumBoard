8358f274b95f19a8d823add18ea46ef3383a98bf
Luza988 <107258622+Luza988@users.noreply.github.com>
2024-04-20 21:18:27 +0200

    Very Simple Error-Page

--- Project/src/main/java/com/team3/project/Controller/WebController.java
+++ Project/src/main/java/com/team3/project/Controller/WebController.java
@@ -1,10 +1,8 @@
 package com.team3.project.Controller;
 
-import com.team3.project.Classes.Email;
 import com.team3.project.Classes.Profile;
 import com.team3.project.Classes.UserStory;
 import com.team3.project.Faced.LogicToData;
-import org.hibernate.annotations.DialectOverride;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
