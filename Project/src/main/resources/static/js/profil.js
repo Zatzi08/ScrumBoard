e20a201b013f2fcf5294652cd395fbd0430e458b
Luza988 <107258622+Luza988@users.noreply.github.com>
2024-04-27 16:22:01 +0200

    js fix

---
+++ Project/src/main/resources/static/js/profil.js
@@ -0,0 +1,12 @@
+function toggleProfilMenü(){
+    var profilBtn = document.getElementById("profilEditBtn");
+    var profilMenü = document.getElementById("profilEditMenü");
+
+    if(profilMenü.style.display == "none") {
+        profilMenü.style.display = "block";
+    }
+    else {
+        profilMenü.style.display = "none";
+    }
+
+}
