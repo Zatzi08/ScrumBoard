c1c673d16cbf523ee115580ba0b94be194733502
Pow211 <pkrasnovska@gmail.com>
2024-05-05 23:12:36 +0200

    window.alert popup für Rechte

--- Project/src/main/resources/static/js/app.js
+++ Project/src/main/resources/static/js/app.js
@@ -1,8 +1,21 @@
 $(document).foundation()
 
 function emailPopup(){
-    var popup = document.getElementById("emailGesendetPopup");
-    popup.className = "show";
-    setTimeout(function(){
-        popup.className = popup.className.replace("show", ""); }, 3000);
+    var emailInput = document.getElementById("emailInput");
+    var lastAtPos = emailInput.lastIndexOf('@');
+    var email = true;
+
+    if (lastAtPos > 0 && emailInput.indexOf('.', lastAtPos) > lastAtPos) email = true;
+    else email = false;
+
+    if(email == true) {
+        var popup = document.getElementById("emailGesendetPopup");
+        popup.className = "show";
+        setTimeout(function () {
+            popup.className = popup.className.replace("show", "");
+        }, 3000);
+        }
+    else {
+        window.alert("Bitte geben Sie eine gültige Email ein!");
+    }
 }
