9350eaabfde299a96e2b74f1f94764aee51dcec0
HenryF <wx2225@uni-rostock.de>
2024-05-20 14:21:43 +0200

    TB2.B1 und TB9.B1 so weit es geht fertiggestellt

--- Project/src/main/java/com/team3/project/Classes/TaskBoard.java
+++ Project/src/main/java/com/team3/project/Classes/TaskBoard.java
@@ -1,5 +1,7 @@
 package com.team3.project.Classes;
 
+import com.team3.project.DAO.DAOTask;
+import com.team3.project.DAO.DAOTaskList;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -17,6 +19,22 @@ public class TaskBoard extends abstraktDataClasses {
         super(tbID);
         this.name = name;
         this.taskListList = new LinkedList<TaskList>();
+    }
+
+    public void setTaskListsInTaskBoard(List <DAOTaskList> daoTaskLists)throws  Exception{
+        if(daoTaskLists == null) throw new Exception("Null List");
+        if(daoTaskLists.isEmpty()) throw new Exception("Empty List");
+        List <TaskList> taskLists = new LinkedList<TaskList>();
+        daoTaskLists.forEach(x->{
+            try{
+                TaskList taskList = new TaskList(x.getId(), x.getName());
+                taskList.setTasksInTaskList(x.getTasks());
+                taskLists.add(taskList);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        });
+        this.taskListList = taskLists;
     }
 
 }
---
+++ Project/src/main/java/com/team3/project/service/TaskListService.java
@@ -0,0 +1,63 @@
+package com.team3.project.service;
+
+import com.team3.project.Classes.TaskList;
+import com.team3.project.DAO.DAOTask;
+import com.team3.project.DAO.DAOTaskList;
+import com.team3.project.DAO.DAOUser;
+import com.team3.project.DAOService.DAOTaskListService;
+import com.team3.project.DAOService.DAOTaskService;
+import com.team3.project.DAOService.DAOUserStoryService;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TaskListService {
+
+    //TODO: fix in daoTasks.add: baue verknüpfte Nutzer mit ein (momentan auf null)
+    //TODO: DB-Funktion zum erstellen einer TaskListe fehlt
+    public void saveTaskList(TaskList taskList)throws Exception{
+        if(taskList.getID() < -1) throw new Exception("Invalid TaskListID");
+        if(taskList.getID() == -1){
+            //DAOTaskListService.createTaskList(taskList.getName());
+        }else{
+            if(taskList.getTasks() == null) throw new Exception("Empty TaskList");
+            if(taskList.getName() == null) throw new Exception("Null Name");
+            if(taskList.getName().isEmpty()) throw new Exception("Empty Name");
+            List<DAOTask> daoTasks = null;
+            taskList.getTasks().forEach(x->{
+                daoTasks.add(new DAOTask(x.getDescription(),x.getPriorityAsInt(),x.isDone(), x.getDueDateAsString(), x.getTimeNeededG(), x.getTimeNeededA(), DAOTaskListService.getWithTasksById(x.getID()), DAOUserStoryService.getById(x.getID()), null));
+            });
+            DAOTaskListService.updateTasksById(taskList.getID(), daoTasks);
+        }
+    }
+
+    public TaskList getTaskListByID(int tlID) throws Exception{
+        if(tlID < 0) throw new Exception("Invalid TaskListID");
+        DAOTaskList daoTaskList = DAOTaskListService.getById(tlID);
+        if(daoTaskList == null) throw new Exception("TaskList not found");
+        TaskList taskList = new TaskList(tlID,daoTaskList.getName() );
+        try{
+        taskList.setTasksInTaskList(daoTaskList.getTasks());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return taskList;
+    }
+
+    public List<TaskList> getAll() throws Exception{
+        List<DAOTaskList> daoTaskLists= DAOTaskListService.getAll();
+        if(daoTaskLists == null) throw new Exception("No TaskList existent");
+        List<TaskList> taskLists = new LinkedList<TaskList>();
+        daoTaskLists.forEach(x->{
+            TaskList taskList = new TaskList(x.getId(),x.getName());
+            try{
+                taskList.setTasksInTaskList(x.getTasks());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            taskLists.add(taskList);
+        });
+        return  taskLists;
+    }
+
+}
